6227a9bca89f962811d68b3744e4a58a2eee9497
matilia <mathewmatilia95@gmail.com>
2024-03-14 13:21:41 +0300

    initial push to a repo

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Lusitana} from "next/font/google";
+import {Inter} from "next/font/google";
+
+export const lutanist = Lusitana({weight: '400', subsets: ['latin']});
+export const inter = Inter({ subsets: ['latin']});
